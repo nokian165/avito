6d3345a5ed282084e5d048921b2691e4f7c0c6bd
User-PC\User <redchukroma@gmail.com>
2018-05-26 14:23:39 +0300

    Add project files.

---
+++ topface/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(topface.Startup))]
+namespace topface
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
